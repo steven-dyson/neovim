c2759ce144cfb54134def6a2c6be494012975045
Steve Dyson <steven.dyson@pm.me>
2025-01-04 14:36:15 -0500

    feat: using system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.clipboard = "" or "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.expandtab = false -- Use tabs instead of spaces
 vim.opt.tabstop = 4 -- Number of spaces that a tab counts for
 vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.g.lazyvim_prettier_needs_config = true
+vim.g.lazyvim_picker = "telescope"
