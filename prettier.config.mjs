dacf547d241d5d0a243fdd5a2fb9cda9df1f5566
Steve Dyson <steven.dyson@pm.me>
2025-07-13 23:25:55 -0400

    format: prettier for md

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  proseWrap: "always", 
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+};
