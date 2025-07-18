2b013c35c8022ff85c299438c6cf4c671044c355
Steven Dyson <sdyson@hvac.mea.com>
2025-07-17 22:14:19 -0400

    fix: wrong var usage

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -2,8 +2,8 @@ GO_VERSION="${1:-1.24.5}"
 GO_TAR="go${GO_VERSION}.linux-amd64.tar.gz"
 INSTALL_DIR="/usr/local"
 
-wget "https://go.dev/dl/go${GO_TAR}"
+wget "https://go.dev/dl/${GO_TAR}"
 rm -rf /usr/local/go
-tar -C "${INSTALL_DIR}" -xzf "{$GO_TAR}"
+tar -C "${INSTALL_DIR}" -xzf "${GO_TAR}"
 rm "${GO_TAR}"
 echo "✅ Go ${GO_VERSION} installed to ${INSTALL_DIR}/go"
