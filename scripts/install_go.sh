a84dead3c99d3325e6c937d9f52da82422a3d9c7
Steven Dyson <sdyson@hvac.mea.com>
2025-07-17 22:21:47 -0400

    fix: using sudo on go

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -2,8 +2,12 @@ GO_VERSION="${1:-1.24.5}"
 GO_TAR="go${GO_VERSION}.linux-amd64.tar.gz"
 INSTALL_DIR="/usr/local"
 
-wget "https://go.dev/dl/${GO_TAR}"
-rm -rf /usr/local/go
-tar -C "${INSTALL_DIR}" -xzf "${GO_TAR}"
+wget -O "${GO_TAR}" "https://go.dev/dl/${GO_TAR}"
+
+sudo rm -rf "${INSTALL_DIR}/go"
+
+sudo tar -C "${INSTALL_DIR}" -xzf "${GO_TAR}"
+
 rm "${GO_TAR}"
+
 echo "✅ Go ${GO_VERSION} installed to ${INSTALL_DIR}/go"
