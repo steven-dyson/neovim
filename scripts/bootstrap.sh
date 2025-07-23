c04a50ccbb100128dce963bfa0f9da73b16fe669
Steve Dyson <steven.dyson@pm.me>
2025-07-23 02:09:54 -0400

    feat: nerd fonts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -12,6 +12,11 @@ $PKG_MGR update && $PKG_MGR full-upgrade -y
 
 # Some essentials
 # TODO
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/0xProto.zip
+unzip 0xProto.zip -d 0xProto
+mkdir -p "$HOME/.local/share/fonts"
+cp 0xProto/*.ttf "$HOME/.local/share/fonts/"
+fc-cache -fv
 
 # Install languages
 ./scripts/install_languages.sh
