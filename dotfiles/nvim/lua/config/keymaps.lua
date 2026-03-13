d4b48ad0d921bff3ab20c2ab23956cae4ca51acc
Steve Dyson <steven.dyson@pm.me>
2026-03-13 00:08:16 -0400

    feat: rest keybinds

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -15,3 +15,9 @@ vim.keymap.set("n", "<leader>ar", "<cmd>AvanteRefresh<CR>", { desc = "Avante: Re
 
 -- Stop request
 vim.keymap.set("n", "<leader>as", "<cmd>AvanteStop<CR>", { desc = "Avante: Stop" })
+
+-- REST API requests
+vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "Rest: Send request" })
+vim.keymap.set("n", "<leader>rp", "<cmd>Rest run pretty<CR>", { desc = "Rest: Send request (pretty print)" })
+vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<CR>", { desc = "Rest: Send last request" })
+vim.keymap.set("n", "<leader>rh", "<cmd>Rest history<CR>", { desc = "Rest: Open history" })
