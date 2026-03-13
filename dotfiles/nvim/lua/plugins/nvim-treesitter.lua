bf3ccd3c62e4c554d6f5cc63c321ecfaaf1616c9
Steve Dyson <steven.dyson@pm.me>
2026-03-13 00:08:04 -0400

    feat: installed http

--- dotfiles/nvim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,8 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      "templ", -- Add this to enable syntax highlighting
+      "templ",
+      "http",
     },
   },
 }
