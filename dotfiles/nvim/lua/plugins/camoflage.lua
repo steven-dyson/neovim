20d4905f84533617318007c6d0767e14c346c6ec
Steve Dyson <steven.dyson@pm.me>
2026-03-21 13:30:02 -0400

    feat: camofalge for env masking

---
+++ dotfiles/nvim/lua/plugins/camoflage.lua
@@ -0,0 +1,11 @@
+return {
+  "zeybek/camouflage.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    { "<leader>ct", "<cmd>CamouflageToggle<cr>", desc = "Toggle Camouflage" },
+    { "<leader>cr", "<cmd>CamouflageReveal<cr>", desc = "Reveal Line" },
+    { "<leader>cy", "<cmd>CamouflageYank<cr>", desc = "Yank Value" },
+    { "<leader>cf", "<cmd>CamouflageFollowCursor<cr>", desc = "Follow Cursor" },
+  },
+}
