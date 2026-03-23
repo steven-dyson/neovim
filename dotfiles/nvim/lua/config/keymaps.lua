86e0ca6ba95004f6e07b19ea27434eb8691478c2
Steve Dyson <steven.dyson@pm.me>
2026-03-23 00:31:30 -0400

    feat: removed avante keymaps

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -1,21 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Ask questions (buffer or selection)
-vim.keymap.set("n", "<leader>aa", "<cmd>AvanteAsk<CR>", { desc = "Avante: Ask" })
-vim.keymap.set("v", "<leader>aa", "<cmd>AvanteAsk<CR>", { desc = "Avante: Ask (selection)" })
-
--- Apply edits
-vim.keymap.set("n", "<leader>ae", "<cmd>AvanteEdit<CR>", { desc = "Avante: Edit" })
-vim.keymap.set("v", "<leader>ae", "<cmd>AvanteEdit<CR>", { desc = "Avante: Edit (selection)" })
-
--- Re-run last request
-vim.keymap.set("n", "<leader>ar", "<cmd>AvanteRefresh<CR>", { desc = "Avante: Refresh" })
-
--- Stop request
-vim.keymap.set("n", "<leader>as", "<cmd>AvanteStop<CR>", { desc = "Avante: Stop" })
-
 -- REST API requests
 vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "Rest: Send request" })
 vim.keymap.set("n", "<leader>rp", "<cmd>Rest run pretty<CR>", { desc = "Rest: Send request (pretty print)" })
